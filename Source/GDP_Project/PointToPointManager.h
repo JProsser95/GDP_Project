dc1d4a7351fab110c4695f51be5fc07af32749c3
BradleyRedfern <redty11@hotmail.com>
2018-02-02 18:04:28 +0000

    Created the PointToPointManager

---
+++ Source/GDP_Project/PointToPointManager.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Components/PrimitiveComponent.h"
+#include "Engine/World.h"
+#include "PossessableActorComponent.h"
+#include "PointToPointManager.generated.h"
+
+
+UCLASS()
+class GDP_PROJECT_API APointToPointManager : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	APointToPointManager();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditAnywhere)
+	TArray<AActor*> Actors;
+	
+	void AllPointsCollected();
+private:
+	int GetVisibleActors();
+};
