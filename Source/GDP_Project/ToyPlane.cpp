c56e48954806f415ea5c8bd0b58b0de7fba46b9a
Sean <sean@devsean.com>
2018-02-07 15:25:55 +0000

    Pointer Safety
    Plane now checks a material has been successfully loaded before attempting to use the material.

--- Source/GDP_Project/ToyPlane.cpp
+++ Source/GDP_Project/ToyPlane.cpp
@@ -31,11 +31,13 @@ AToyPlane::AToyPlane()
 	//PlaneBodyMeshComponent->OnComponentBeginOverlap.AddDynamic(this, &AToyPlane::OnToyPlaneOverlap);
 	//PlaneBodyMeshComponent->BodyInstance.SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics, true);
 	static ConstructorHelpers::FObjectFinder<UMaterialInterface> MaterialPlane(TEXT("MaterialInstanceDynamic'/Game/Plane/Texture/Plane_Material.Plane_Material'"));
+	if (MaterialPlane.Object)
+		PlaneBodyMeshComponent->SetMaterial(0, MaterialPlane.Object);
 
 	static ConstructorHelpers::FObjectFinder<UStaticMesh> MeshAssetBody(TEXT("StaticMesh'/Game/Plane/Plane_Plane.Plane_Plane'"));
 	if (MeshAssetBody.Object)
 		PlaneBodyMeshComponent->SetStaticMesh(MeshAssetBody.Object);
-	PlaneBodyMeshComponent->SetMaterial(0, MaterialPlane.Object);
+
 	RootComponent = PlaneBodyMeshComponent;
 
 	//Mesh
