ff3f79f4ed33823aae888f37e239758123310c00
Sean <sean@devsean.com>
2018-02-14 17:49:22 +0000

    Revised Plane & Car
    Plane now has a realistic option which needs testing.
    Car's wheels do not drop as far when the car is in the air.
    Wheels correctly fall off the edge.
    Wheels no longer clip through walls.
    The car no longer floats.
    Modified car assets.
    Plane is closer to the car in the plane test level for quicker testing.
    Car can no longer reset as fast as the user presses a key, stopping players flying through levels.
    Attempted a rotation cap on the car, however due to buggy behaviour this has been left commented out.
    Plane pitch is now half the prior speed (needs testing).

--- Source/GDP_Project/ToyCar.cpp
+++ Source/GDP_Project/ToyCar.cpp
@@ -29,7 +29,8 @@
 const FName AToyCar::LookUpBinding("LookUp");
 const FName AToyCar::LookRightBinding("LookRight");
 
-AToyCar::AToyCar() 
+AToyCar::AToyCar()
+	:RespawnDelay(1.5f)
 {
 	// Car mesh
 	static ConstructorHelpers::FObjectFinder<USkeletalMesh> CarMesh(TEXT("SkeletalMesh'/Game/Car/TOYCAR.TOYCAR'"));
@@ -56,23 +57,33 @@ AToyCar::AToyCar()
 
 	check(Vehicle4W->WheelSetups.Num() == 4);
 
+
+	float RearForwardDistance(24.0f);
+	float FrontForwardDistance(-18.0f);
+
+	float FrontSideDistance(8.0f);
+	float RearSideDistance(10.0f);
+
+	float FrontWheelHeight(5.75f);
+	float RearWheelHeight(4.0f);
+
 	// Wheels/Tyres
 	// Setup the wheels
 	Vehicle4W->WheelSetups[0].WheelClass = UToyCarWheelFront::StaticClass();
 	Vehicle4W->WheelSetups[0].BoneName = FName("WheelFL");
-	Vehicle4W->WheelSetups[0].AdditionalOffset = FVector(0.f, -8.f, 0.f);
+	Vehicle4W->WheelSetups[0].AdditionalOffset = FVector(FrontForwardDistance, -FrontSideDistance, FrontWheelHeight);
 
 	Vehicle4W->WheelSetups[1].WheelClass = UToyCarWheelFront::StaticClass();
 	Vehicle4W->WheelSetups[1].BoneName = FName("WheelFR");
-	Vehicle4W->WheelSetups[1].AdditionalOffset = FVector(0.f, 8.f, 0.f);
+	Vehicle4W->WheelSetups[1].AdditionalOffset = FVector(FrontForwardDistance, FrontSideDistance, FrontWheelHeight);
 
 	Vehicle4W->WheelSetups[2].WheelClass = UToyCarWheelRear::StaticClass();
 	Vehicle4W->WheelSetups[2].BoneName = FName("WheelRL");
-	Vehicle4W->WheelSetups[2].AdditionalOffset = FVector(0.f, -8.f, 0.f);
+	Vehicle4W->WheelSetups[2].AdditionalOffset = FVector(RearForwardDistance, RearSideDistance, RearWheelHeight);
 
 	Vehicle4W->WheelSetups[3].WheelClass = UToyCarWheelRear::StaticClass();
 	Vehicle4W->WheelSetups[3].BoneName = FName("WheelRR");
-	Vehicle4W->WheelSetups[3].AdditionalOffset = FVector(0.f, 8.f, 0.f);
+	Vehicle4W->WheelSetups[3].AdditionalOffset = FVector(RearForwardDistance, -RearSideDistance, RearWheelHeight);
 
 	// Adjust the tire loading
 	Vehicle4W->MinNormalizedTireLoad = 0.0f;
@@ -205,8 +216,26 @@ void AToyCar::Tick(float DeltaTime)
 	cam.Roll = 0;
 	Camera->SetWorldRotation(cam);
 
-	// Update phsyics material
+	//float rotationCap(85.0f);
+
+	// Update physics material
 	//UpdatePhysicsMaterial();
+
+	FRotator currentRotation(GetActorRotation());
+
+	// Cap pitch
+	//if (currentRotation.Pitch > rotationCap)
+	//	currentRotation.Pitch = rotationCap;
+	//else if (currentRotation.Pitch < -rotationCap)
+	//	currentRotation.Pitch = -rotationCap;
+	//
+	//// Cap roll
+	//if (currentRotation.Roll > rotationCap)
+	//	currentRotation.Roll = rotationCap;
+	//else if (currentRotation.Roll < -rotationCap)
+	//	currentRotation.Roll = -rotationCap;
+	//
+	//SetActorRotation(currentRotation, ETeleportType::TeleportPhysics);
 }
 
 void AToyCar::Restart()
@@ -369,22 +398,46 @@ void AToyCar::OnEndOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherAct
 
 void AToyCar::ResetPositionAndRotation()
 {
-	FVector currentLoaction = this->GetActorLocation();
-	FRotator currentRotation = this->GetActorRotation();
+	static float lastRespawn(0.0f);
 
-	this->SetActorRelativeLocation(FVector(currentLoaction.X, currentLoaction.Y, currentLoaction.Z + 100), false, NULL, ETeleportType::TeleportPhysics);
-	this->SetActorRotation(FRotator(0,0,0), ETeleportType::TeleportPhysics);
+	if (GetWorld()->GetTimeSeconds() - lastRespawn >= RespawnDelay)
+	{
+		lastRespawn = GetWorld()->GetTimeSeconds();
+
+		FVector currentLoaction = this->GetActorLocation();
+		FRotator currentRotation = this->GetActorRotation();
+
+		this->SetActorRelativeLocation(FVector(currentLoaction.X, currentLoaction.Y, currentLoaction.Z + 100), false, NULL, ETeleportType::TeleportPhysics);
+		this->SetActorRotation(FRotator(0.0f, currentRotation.Yaw, 0.0f), ETeleportType::TeleportPhysics);
+
+		//UWheeledVehicleMovementComponent4W* Vehicle4W = CastChecked<UWheeledVehicleMovementComponent4W>(GetVehicleMovement());
+		//Vehicle4W->Velocity = FVector(0.0f, 0.0f, 0.0f);
+		//RootComponent->ComponentVelocity = FVector(0.0f, 0.0f, 0.0f);
+	}
 }
 
 void AToyCar::Respawn()
 {
-	for (TActorIterator<ARespawnPoint> ActorItr(GetWorld()); ActorItr; ++ActorItr)
+	static float lastRespawn(0.0f);
+
+	if (GetWorld()->GetTimeSeconds() - lastRespawn >= RespawnDelay)
 	{
-		if (ActorItr->GetIsCurrentSpawnPoint())
+		lastRespawn = GetWorld()->GetTimeSeconds();
+
+		for (TActorIterator<ARespawnPoint> ActorItr(GetWorld()); ActorItr; ++ActorItr)
 		{
-			FVector spawnLocation = ActorItr->GetActorLocation();
-			spawnLocation.Z += 100;
-			this->SetActorLocationAndRotation(spawnLocation, FRotator(0, 0, 0), false, NULL, ETeleportType::TeleportPhysics);
+			if (ActorItr->GetIsCurrentSpawnPoint())
+			{
+				FRotator currentRotation = this->GetActorRotation();
+
+				FVector spawnLocation = ActorItr->GetActorLocation();
+				spawnLocation.Z += 100;
+				this->SetActorLocationAndRotation(spawnLocation, FRotator(0.0f, currentRotation.Yaw, 0.0f), false, NULL, ETeleportType::TeleportPhysics);
+				GetWorld()->GetTimeSeconds();
+				//UWheeledVehicleMovementComponent4W* Vehicle4W = CastChecked<UWheeledVehicleMovementComponent4W>(GetVehicleMovement());
+				//Vehicle4W->Velocity = FVector(0.0f, 0.0f, 0.0f);
+				//RootComponent->ComponentVelocity = FVector(0.0f, 0.0f, 0.0f);
+			}
 		}
 	}
 }
